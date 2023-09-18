8a47e63a8b28a780cc2106e833bc7298ff0ce946
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-18 12:40:16 +0600

    solve new-page issue

--- index.mjs
+++ index.mjs
@@ -1,12 +1,13 @@
 import puppeteer from "puppeteer";
 
-const browser = await puppeteer.launch({ headless: "false" });
+const browser = await puppeteer.launch({ headless: false });
 const page = await browser.newPage();
 
 await page.goto("https://alameda-fluid-demo.squarespace.com/", {
-  waitUntil: "networkidle2",
+  waitUntil: "networkidle0",
 });
 await page.waitForSelector(".product-lists-item");
+
 const productLinks = await page.evaluate(() => {
   return [...document.querySelectorAll(".product-lists-item")].map(
     (e) => e.href
@@ -14,21 +15,6 @@ const productLinks = await page.evaluate(() => {
 });
 
 console.log(productLinks);
-// await page.close();
-// await browser.close();
 
-for (const productLink of productLinks) {
-  const page = await browser.newPage();
-  await page.goto(productLink, { waitUntil: "networkidle2" });
-  await page.waitForSelector(".grid-title");
-  const title = await page.evaluate(() => {
-    return document.querySelector(".grid-title")?.innerHTML;
-  });
-
-  const price = await page.evaluate(() => {
-    return document.querySelector(".product-price")?.innerHTML;
-  });
-
-  console.log(productLink, title, price);
-  await page.close();
-}
+await page.click();
+await browser.close();
