2ff1aaab2f05a076445ba38954303bd336e5d29d
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-18 15:18:28 +0600

    fix some errors

--- index.mjs
+++ index.mjs
@@ -1,36 +1,76 @@
+// import puppeteer from "puppeteer";
+
+// const browser = await puppeteer.launch({ headless: false });
+// const page = await browser.newPage();
+
+// await page.goto("https://alameda-fluid-demo.squarespace.com/", {
+//   waitUntil: "networkidle0",
+// });
+// await page.waitForSelector(".product-lists-item");
+
+// const productLinks = await page.evaluate(() => {
+//   return [...document.querySelectorAll(".product-lists-item")].map(
+//     (e) => e.href
+//   );
+// });
+
+// console.log(productLinks);
+
+// await page.close();
+// // await browser.close();
+
+// for (const productLink of productLinks) {
+//   const page = await browser.newPage();
+//   await page.goto(productLink, { waitUntil: "networkidle0" });
+
+//   await page.waitForSelector(".grid-title");
+//   const title = page.evaluate(() => {
+//     return document.querySelector(".grid-title")?.innerHTML;
+//   });
+
+//   const price = page.evaluate(() => {
+//     return document.querySelector(".product-price")?.innerHTML;
+//   });
+//   console.log(productLink, title, price);
+//   await page.close();
+// }
+
 import puppeteer from "puppeteer";
 
-const browser = await puppeteer.launch({ headless: false });
-const page = await browser.newPage();
-
-await page.goto("https://alameda-fluid-demo.squarespace.com/", {
-  waitUntil: "networkidle0",
-});
-await page.waitForSelector(".product-lists-item");
-
-const productLinks = await page.evaluate(() => {
-  return [...document.querySelectorAll(".product-lists-item")].map(
-    (e) => e.href
-  );
-});
-
-console.log(productLinks);
-
-await page.click();
-// await browser.close();
-
-for (const productLink of productLinks) {
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto(productLink, { waitUntil: "networkidle0" });
 
-  await page.waitForSelector(".grid-title");
-  const title = page.evaluate(() => {
-    return document.querySelector(".grid-title")?.innerHTML;
+  await page.goto("https://alameda-fluid-demo.squarespace.com/", {
+    waitUntil: "networkidle0",
+  });
+  await page.waitForSelector(".product-lists-item");
+
+  const productLinks = await page.evaluate(() => {
+    return [...document.querySelectorAll(".product-lists-item")].map(
+      (e) => e.href
+    );
   });
 
-  const price = page.evaluate(() => {
-    return document.querySelector(".product-price")?.innerHTML;
-  });
-  console.log(productLink, title, price);
+  console.log(productLinks);
+
   await page.close();
-}
+
+  for (const productLink of productLinks) {
+    const page = await browser.newPage();
+    await page.goto(productLink, { waitUntil: "networkidle0" });
+
+    await page.waitForSelector(".grid-title");
+    const title = await page.evaluate(() => {
+      return document.querySelector(".grid-title")?.innerHTML;
+    });
+
+    const price = await page.evaluate(() => {
+      return document.querySelector(".product-price")?.innerHTML;
+    });
+    console.log(productLink, title, price);
+    await page.close();
+  }
+
+  await browser.close(); // Close the browser when done.
+})();
